2d810202f9466a99eb75b3e7473f2c0d9228b470
jfedonenko <70053044+jfedonenko@users.noreply.github.com>
2021-03-03 10:30:36 +0200

    1
    1

--- php17/index.php
+++ php17/index.php
@@ -1,4 +1,15 @@
 
+<?php
+require_once  __DIR__ . "/vendor/autoload.php";
+
+//подключаем класс Connect
+use App\Connect;
+
+//для проверки БД $bd Connect делаем код
+if (!Connect::check()){
+    die("-----Ошибка в классе Connect - База Данных-----");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <?php require_once "inclides/head.php"?>
@@ -21,19 +32,35 @@
 					</span>
             </div>
             <!------ БЭМ БЛОК для формы------>
-            <div class="reg">
+             <div class="reg">
+
+
+                <?php
+
+                $rooms = mysqli_query(Connect::db(), "SELECT * FROM `rooms`");
+
+                while ($rooms = mysqli_fetch_assoc($rooms)){
+
+
+                ?>
                 <h6 class="reg__title">Список комнат</h6>
                 <p class="reg__text">
-                   Количество участников 92
+                    Количество участников 92
                 </p>
                 <form action="#" class="reg__form">
                     <input type="text" name="name" class="reg__input input"placeholder="Ваше имя" required>
-                   
+
                     <button type="submit" class="reg__btn button"> Войти</button>
                 </form>
                 <p class="reg__text reg__text_tiny">
                     Ваши данные под защитой
                 </p>
+
+                <?php
+
+
+                }
+                ?>
             </div>
         </div>
     </div>
