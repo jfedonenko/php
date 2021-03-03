007b440e29d72d7d6c1d26b4be1e42cfa22f3ce2
jfedonenko <70053044+jfedonenko@users.noreply.github.com>
2021-03-03 10:59:43 +0200

    1
    1

--- php17/index.php
+++ php17/index.php
@@ -1,4 +1,3 @@
-
 <?php
 require_once  __DIR__ . "/vendor/autoload.php";
 
@@ -15,44 +14,47 @@ if (!Connect::check()){
 <?php require_once "inclides/head.php"?>
 <body>
 <?php require_once "inclides/header.php"?>
-    <div class="offer">
-         <div class="container text-align-center align-items-center">
-             <div class="reg">
+<div class="offer">
+    <div class="container text-align-center align-items-center">
+        <h2> Список комнат:</h2>
 
 
-                <?php
+<ul class="reg rooms">
 
-                $rooms = mysqli_query(Connect::db(), "SELECT * FROM `rooms`");
+    <?php
+    $rooms = mysqli_query(Connect::db(), "SELECT * FROM `rooms`");
+    while ($room = mysqli_fetch_assoc($rooms)){
+    ?>
 
-                while ($room = mysqli_fetch_assoc($rooms)){
+    <li class="reg__title">
+        <h6 class="reg__title">
+
+            <?= $room["title"] ?>
+
+        </h6>
+        <p class="reg__text">
+            Количество участников 92
+        </p>
+        <form action="#" class="reg__form">
+            <input type="text" name="name" class="reg__input input"placeholder="Ваше имя" required>
+
+            <button type="submit" class="reg__btn button"> Войти</button>
+        </form>
+        <p class="reg__text reg__text_tiny">
+            Ваши данные под защитой
+        </p>
+    </li>
+        <?php
 
 
-                ?>
-                 <h6 class="reg__title">
-                    
-                  <?= $room["title"] ?>
+    }
+    ?>
+</ul>
 
-                </h6>
-                <p class="reg__text">
-                    Количество участников 92
-                </p>
-                <form action="#" class="reg__form">
-                    <input type="text" name="name" class="reg__input input"placeholder="Ваше имя" required>
+              
 
-                    <button type="submit" class="reg__btn button"> Войти</button>
-                </form>
-                <p class="reg__text reg__text_tiny">
-                    Ваши данные под защитой
-                </p>
-
-                <?php
-
-
-                }
-                ?>
-            </div>
-        </div>
     </div>
+</div>
 </header>
 
 </body>
