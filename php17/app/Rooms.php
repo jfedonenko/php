218b5ed56a79cb9a3c83ec2ccc154ed44763c642
jfedonenko <70053044+jfedonenko@users.noreply.github.com>
2021-03-03 14:25:35 +0200

    1
    1

---
+++ php17/app/Rooms.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App;
+class Rooms extends  Connect
+{
+    public static function create($name)
+    {
+        //делаем проверку, если поле пусто
+        if ($name === "") {
+            return false;
+        }
+        //подключаем $SQL
+        $SQL = "INSERT INTO `rooms` (`id`, `title`) VALUES (NULL, '$name')";
+        //подключаем базу данных  из класса Connect db()
+        $room = mysqli_query(self::db(), $SQL);
+
+        //проверяем
+        return $room ? true : false;
+    }
+
+    public static function addMembers($name, $room_id)
+    {
+        if ($name === "" || $room_id === "") {
+            return false;
+        }
+            //подключаем $SQL
+            $SQL = "INSERT INTO `members` (`id`, `name`, `room_id`) VALUES (NULL,'$name,', '$room_id')";
+
+        $connect = self::db();
+        //подключаем базу данных  из класса Connect db()
+            $member = mysqli_query(self::db(), $SQL);
+            //проверяем
+            return $member ? mysqli_insert_id($connect) : false;
+        }
+        public static function clickMemderRoom($room_id, $member_id)
+            {
+
+         $room = mysqli_fetch_assoc(mysqli_query(self::db(),"SELECT * FROM `rooms` WHERE 'id' = $room_id"));
+
+            if(is_null($room)){
+            return false;
+            }
+
+            $member = mysqli_fetch_assoc(mysqli_query(self::db(),"SELECT * FROM `member` WHERE 'id' = $room_id"));
+                if(is_null($member)){
+                return false;
+            }
+                if ((int)$member["room_id"] !==(int)$room["id"]){
+                    return false;
+                }
+        return true;
+        }
+    }
